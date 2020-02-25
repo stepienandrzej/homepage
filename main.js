5cd025b1b42e6a3a1af4f87201130e901c0b9807
Andrzej Stepien <stepienandrzej41@gmail.com>
2020-02-25 22:15:35 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('siema');
+const name = 'Andrzej';
+const age = 39;
+console.log(name);
+console.log(age);
+
+console.log(`nazywam sie ${name} i mam ${age} lat`);
---
+++ mane.js
@@ -0,0 +1,7 @@
+console.log('witaj nieznajomy');
+const nazwa = 'Lukasz';
+const wiek = 40;
+console.log(nazwa);
+console.log(wiek);
+
+console.log(`nazywam sie ${nazwa} i mam ${wiek} lat`);
